958af427f23dd55f4cda31924f82163e6c90eeba
Anna Lyvia Medeiros <santosannslyvia@gmail.com>
2022-09-12 21:52:15 -0300

    Subindo exercicio referente a Herança.

---
+++ Ted_Heranca/Main.java
@@ -0,0 +1,26 @@
+package Ted_Heranca;
+
+public class Main {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Lucas Lucena", "01/10/1992", "Avenida Brasil", "3247-2882", "Estudante", 202120222, 50000, "01/01/2020");
+        Cliente cliente = new Cliente("Mateus Correia", "08/05/2001", "Avenida Sei lá", "4002-8922", "Dev", "007");
+
+        System.out.println("Dados do Funcionário");
+        System.out.println("Nome: " + funcionario.getNome());
+        System.out.println("Data de Nascimento: " + funcionario.getDataNascimento());
+        System.out.println("Endereço: " + funcionario.getEndereco());
+        System.out.println("Telefone para contato: " + funcionario.getTelsContato());
+        System.out.println("Cargo: " + funcionario.getCargo());
+        System.out.println("Matrícula: " + funcionario.getMatricula());
+        System.out.println("Salário: " + funcionario.getSalario());
+        System.out.println("Data de Admissão: " + funcionario.getDataAdmissao());
+
+        System.out.println("\n\nDados do Cliente");
+        System.out.println("Nome: " + cliente.getNome());
+        System.out.println("Data de Nascimento: " + cliente.getDataNascimento());
+        System.out.println("Endereço: " + cliente.getEndereco());
+        System.out.println("Telefone para contato: " + cliente.getTelsContato());
+        System.out.println("Cargo: " + cliente.getCargo());
+        System.out.println("Código: " + cliente.getCodigo());
+    }
+}
---
+++ Ted_Heranca/Pessoa.java
@@ -0,0 +1,71 @@
+package Ted_Heranca;
+
+public class Pessoa {
+    protected String nome;
+    protected String dataNascimento;
+    protected String endereco;
+    protected String telsContato; // adicionar coleção
+    protected String cargo;
+
+    public Pessoa(String nome, String dataNascimento, String endereco, String telsContato, String cargo) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+        this.telsContato = telsContato;
+        this.cargo = cargo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelsContato() {
+        return telsContato;
+    }
+
+    public void setTelsContato(String telsContato) {
+        this.telsContato = telsContato;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void cadastrar(){
+
+    }
+
+    public void obterIdade(){
+
+    }
+
+    public void reajustarSalario(double percentual, double real){
+    }
+
+    public void promover(String novoCargo, String cargo){
+    }
+}
